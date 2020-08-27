20442d890f3bd5cf4b20b6439707eda69ee63047
Jokasxy <josip_1997@live.com>
2020-08-27 18:55:16 +0200

    Additional seeds

--- database/seeds/ImageUrlsSeeder.php
+++ database/seeds/ImageUrlsSeeder.php
@@ -20,6 +20,7 @@ class ImageUrlsSeeder extends Seeder
             'imageURL' => 'https://www.arcgis.com/sharing/rest/content/items/d02e2cd2e46a40aeae3661b2044a3b0a/resources/1589545644386.jpeg?w=1600',
             'location_id' => '1',
         ]);
+
         DB::table('image_urls')->insert([
             'imageURL' => 'https://www.planetware.com/wpimages/2019/06/czech-republic-prague-itineraries-for-travelers-one-day-itinerary-old-town-square.jpg',
             'location_id' => '2',
@@ -27,6 +28,100 @@ class ImageUrlsSeeder extends Seeder
         DB::table('image_urls')->insert([
             'imageURL' => 'https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/Czech%20Republic/Prague/prague-main-image.jpg',
             'location_id' => '2',
+        ]);
+
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1920px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg',
+            'location_id' => '3',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Louvre_Museum_Wikimedia_Commons.jpg/1920px-Louvre_Museum_Wikimedia_Commons.jpg',
+            'location_id' => '3',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Conseil_d%27Etat_Paris_WA.jpg/1920px-Conseil_d%27Etat_Paris_WA.jpg',
+            'location_id' => '3',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Rue_de_Rivoli_at_night%2C_Paris_August_2013.jpg/1280px-Rue_de_Rivoli_at_night%2C_Paris_August_2013.jpg',
+            'location_id' => '3',
+        ]);
+
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://i.insider.com/5d0150e06fc9200b5a05f042?width=1136&format=jpeg',
+            'location_id' => '4',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.gannett-cdn.com/media/2018/12/15/USATODAY/usatsports/MotleyFool-TMOT-657f0436-21e9af86.jpg?quality=50',
+            'location_id' => '4',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://cdn.getyourguide.com/img/location/5c04f6f20650d.jpeg/148.jpg',
+            'location_id' => '4',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://cdn.vox-cdn.com/thumbor/l7KiYMM-47Ktsd7zRd3jffBilCo=/0x0:6000x4000/1200x800/filters:focal(2520x1520:3480x2480)/cdn.vox-cdn.com/uploads/chorus_image/image/66907018/shutterstock_1701722863.0.jpg',
+            'location_id' => '4',
+        ]);
+
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTYyNDg1MjE3MTI1Mjc5Mzk4/topic-london-gettyimages-760251843-promo.jpg',
+            'location_id' => '5',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.cityam.com/wp-content/uploads/2020/02/London_Tower_Bridge_City.jpg',
+            'location_id' => '5',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/United%20Kingdom/London/london-aerial-thames-guide-xlarge.jpg',
+            'location_id' => '5',
+        ]);
+
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://media.timeout.com/images/105240189/630/472/image.jpg',
+            'location_id' => '6',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://gotravel.hr/wp-content/uploads/2019/01/tirta-gangga.png',
+            'location_id' => '6',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.popustplus.hr/upload/groupbuydeal/92116/otok-bali-10-dana-popustplus0_5ac38d68898ab.jpg',
+            'location_id' => '6',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.integral-zagreb.hr/sites/default/files/styles/1920_auto_/public/uploads/products/gallery/2019-11/bali-putovanje-3.jpg?itok=qcAVezhd',
+            'location_id' => '6',
+        ]);
+
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://gotravel.hr/wp-content/uploads/2017/06/Rio-de-Janeiro-an-amazing-part-in-brazil.jpg',
+            'location_id' => '7',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.wish.hr/wp-content/uploads/2012/01/rio-janeiro.jpg',
+            'location_id' => '7',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://www.mondotravel.hr/assets/images/0d/0d8a2c_Brazil---Copacabana-and-Ipanema-beaches-in-Rio-De-Janeiro..jpg',
+            'location_id' => '7',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://images.vinko.me/eyJidWNrZXQiOiJ0cmF2ZWwtYWR2aXNvci1pbWFnZXMiLCJrZXkiOiJ1cGxvYWRzLzIwMTgvMTAvcmlvMy1lMTU0MDQ5ODkzMjY0Ni5qcGcifQ==',
+            'location_id' => '7',
+        ]);
+
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://d2bgjx2gb489de.cloudfront.net/gbb-blogs/wp-content/uploads/2018/03/06190153/Istanbul-1.jpg',
+            'location_id' => '8',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://cdn.getyourguide.com/img/tour/5c5c25ee018b0.jpeg/146.jpg',
+            'location_id' => '8',
+        ]);
+        DB::table('image_urls')->insert([
+            'imageURL' => 'https://image.dnevnik.hr/media/images/567x350/Feb2020/61837059-istanbul-turska.jpg',
+            'location_id' => '8',
         ]);
     }
 }
--- database/seeds/LocationsSeeder.php
+++ database/seeds/LocationsSeeder.php
@@ -26,5 +26,47 @@ class LocationsSeeder extends Seeder
             'latitude' => 50.08804,
             'longitude' => 14.42076,
         ]);
+        DB::table('locations')->insert([
+            'name' => 'Paris',
+            'country' => 'France',
+            'description' => 'Paris is the capital city of France, the capital of love and one of the principal tourist centers in modern Europe. The city is located in the region known as Île-de-France which can be found in the north-central part of the country. For many centuries Paris has been considered to be a cultural and fashion capital of the continent, with a large number of fashion designers, makeup and beauty specialists, artists, cinema and music related specialists living and working in the city.',
+            'latitude' => 48.864716,
+            'longitude' => 2.349014,
+        ]);
+        DB::table('locations')->insert([
+            'name' => 'New York',
+            'country' => 'NY, United States of America',
+            'description' => 'New York City is one of the most known cities-symbols of the USA located in the northwestern part of the country, on the Atlantic coast. It is known as a global business capital city and one of the most populous, developed, innovative, and bustling cities of the world. New York is a true wonder of the US, and it can open its secrets and beautiful atmosphere to anyone who comes to visit it. It is located on the mouth of the Hudson River and is a destination for all kinds of tourists and guests, from business people to various tourists seeking some rest and new adventures.',
+            'latitude' => 40.730610,
+            'longitude' => -73.935242,
+        ]);
+        DB::table('locations')->insert([
+            'name' => 'London',
+            'country' => 'England, United Kingdom',
+            'description' => 'London is a political, historical, cultural, and tourist center of the United Kingdom, an important city and commercial spot in Western Europe, as well as one of the largest European cities, located on the southeastern part of the British Island. London is a heart of European tourism, with annual spending of over16 billion pounds for maintaining and renovating its attractions, adding new amazing entertainment and cultural objects and establishments. There are a few districts in the city, including its business heart Soho, governmental district of Westminster, industrial East End, etc.',
+            'latitude' => 51.509865,
+            'longitude' => -0.118092,
+        ]);
+        DB::table('locations')->insert([
+            'name' => 'Bali',
+            'country' => 'Indonesia',
+            'description' => 'An island and a province in Indonesia, Bali is one of the most known luxury resorts and a developed tourist area in the Far East. Bali is located to the east of the island of Java and is connected with the large island by a frequent ferry line. For many decades Bali has been among the most popular seaside vacation destinations for people from all around the world and every year millions of tourists and travelers visit the island which offers amazing opportunities for all kinds of vacation types, for all tastes and budgets. There are a few million people living currently in Bali, and most of those work on the field of services, including accommodation, catering, recreation, health care, and so on. It is quite easy to reach the island by air and the local airport connects the island with many countries of the planet.',
+            'latitude' => -8.409518,
+            'longitude' => 115.188919,
+        ]);
+        DB::table('locations')->insert([
+            'name' => 'Rio de Janeiro',
+            'country' => 'Brasil',
+            'description' => 'Rio de Janeiro is a large coastal city and one of the most popular tourist destinations, as well as the capital of Rio de Janeiro State, in south eastern Brazil. With the population of this metropolitan area close to 12 million people, it is the second largest settlement of the country after Sao Paulo. Rio de Janeiro is a principal center of cultural and business life of the country, but especially a tourist spot, with millions and millions of people visiting the city year round, but especially during the season of traditional Brazilian carnivals. The main attractions of the city are very well known and include the Christ the Redeemer, Copacabana Beach, Pedra da Gávea mountain, Sugarloaf Mount, numerous beaches, markets, and shopping areas, as well as Marakana Stadium and other facilities for sports.',
+            'latitude' => -22.908333,
+            'longitude' => -43.196388,
+        ]);
+        DB::table('locations')->insert([
+            'name' => 'Istanbul',
+            'country' => 'Turkey',
+            'description' => 'Though sometimes it is erroneously named as the capital of Turkey, Istanbul is the main cultural and geopolitical center, at that is one of the largest and most populous cities of the Middle East. It is located in the northwestern part of the country, on the Strait of Bosporus, right between the Black Sea and the Marmara Sea. Istanbul, with its amount of historic monuments and cultural events, is a dream city for those who like traveling, learning new cultures and old historic traditions. The history of the city starts in the post-Roman Empire times, and the city used to play a key role as the capital of the Byzantine Empire, known as Constantinople. It used to be the final point of the so-called Silk Road stretching from the city of Shanghai to modern Istanbul.',
+            'latitude' => 41.015137,
+            'longitude' => 28.979530,
+        ]);
     }
 }
