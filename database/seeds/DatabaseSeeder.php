807ee6b56d9fda35b597cebd0b93946538a6dba3
Josip Blažević <jblazevic@binaryx.hr>
2020-09-03 20:20:16 +0200

    Class name typo fixed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LocationsSeeder::class);
-        $this->call(ImageURLsSeeder::class);
+        $this->call(ImageUrlsSeeder::class);
     }
 }
